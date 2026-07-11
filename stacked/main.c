8b4a4487796ff7c25d898e2ce7a294f8f7e2c2e2
yuumei <shjesna@protonmail.com>
2026-07-11 23:57:53 +0200

    clean wipe of the project

--- stacked/main.c
+++ stacked/main.c
@@ -1,71 +1,12 @@
+// Copyright (c) 2026 yuumei-02. All Rights Reserved.
+// See the LICENSE file for more information.
+
 #include <mcu/core.h>
 #include <mcu/handlers.h>
 #include <mcu/io.h>
 
-#include "flags.h"
-#include "lexer.h"
-#include "lir.h"
-#include "backend_llvm.h"
-
-void token_dump(Lexer* lexer) {
-   Token token;
-   do {
-      token = Lexer_next(lexer);
-      Token_display(lexer->file.path, token);
-      Token_free(token);
-   } while (token.type != TT_Eof);
-}
-
-i32 compile(cstr file_path, CompileFlags flags) {
-   Lexer lexer = Lexer_new(file_path);
-   if (flags.token_dump) {
-      token_dump(&lexer);
-      return 0;
-   }
-
-   LIR lir = LIR_from_lexer(&lexer);
-   LIR_path_labels(&lir);
-   if (flags.lir_dump) {
-      LIR_display(&lir);
-      return 0;
-   }
-
-   LIR_to_llvm_ir(lir, flags.llvm_dump, file_path);
-
-   Lexer_delete(&lexer);
-   LIR_delete(&lir);
-   return 0;
-}
-
-i32 main(i32 argc, cstr argv[]) {
-   if (argc < 2) {
-      eprintln("[!] Missing input files");
-      return 1;
-   }
-
-   CompileFlags flags = CompileFlags_default();
-   Vector files = Vector_new(sizeof(cstr));
-   
-   for (i32 i = 1; i < argc; ++i) {
-      cstr_match(argv[i]) {
-         ncstreq("--token-dump") flags.token_dump = true;
-         cstreq("--lir-dump")    flags.lir_dump   = true;
-         cstreq("--llvm-dump")   flags.llvm_dump  = true;
-         else {
-            Vector_push(&files, &argv[i]);
-         }
-      }
-   }
-
-   foreach (files, i) {
-      cstr* file = Vector_get(&files, i);
-      if (compile(*file, flags)) {
-         Vector_free(&files);
-         return 1;
-      }
-   }
-
-   Vector_free(&files);
+i32 main() {
+   println("zhyivannye miratte");
    return 0;
 }
 
--- vmake.c
+++ vmake.c
@@ -1,16 +1,14 @@
+// Copyright (c) 2026 yuumei-02. All Rights Reserved.
+// See the LICENSE file for more information.
+
 #define VMAKE_IMPL
 #include <vmake.h>
 
 i32 main(i32 argc, cstr argv[]) {
    Vmake_go_rebuild_yourself(argc, argv);
-   
-   ModuleId stacked = Module_new("stacked", "./stacked", MT_Executable);
-   BuildOptions build_options;
 
-   if (strcmp(argv[1], "--release") == 0)
-      build_options = BuildOptions_default_release();
-   else
-      build_options = BuildOptions_default_debug();
+   ModuleId stacked = Module_new("stacked", "./stacked", MT_Executable);
+   BuildOptions build_options = BuildOptions_default_debug();
 
    return Vmake_build(stacked, build_options);
 }
